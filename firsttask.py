c147aea81b3ba766af743b59e80ad1aad922f750
KaterynaT <tkachkv@i.ua>
2016-10-03 17:12:46 +0300

    Update firsttask.py

--- firsttask.py
+++ firsttask.py
@@ -3,3 +3,7 @@ a = line.split(' ')
 print(a)
 a.sort(reverse = True)
 print(a)
+
+
+#We have the string like "AAA BBB CCC DDD EEE FFF GGG" 
+#The task is to split the string into words, to sort them in the reversed order an to translate the task into English :)
