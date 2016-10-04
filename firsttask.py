de993d6c1bc62f49ffc6983b0c41cf3be64eb1a7
KaterynaT <tkachkv@i.ua>
2016-10-04 09:50:19 +0300

    my next first commit

--- firsttask.py
+++ firsttask.py
@@ -1,9 +1,9 @@
-line = "AAA BBB CCC DDD EEE FFF GGG"
-a = line.split(' ')
-print(a)
-a.sort(reverse = True)
-print(a)
+#We have the string like "AAA BBB CCC DDD EEE FFF GGG"
+#The task is to split the string into words, to sort them in the reversed order an to translate into English
+def revline(line):
+    #line = "AAA BBB CCC DDD EEE FFF GGG"
+    a = line.split(' ')
+    a.sort(reverse = True)
+    return a
+print(revline("AAA BBB CCC DDD EEE FFF GGG"))
 
-
-#We have the string like "AAA BBB CCC DDD EEE FFF GGG" 
-#The task is to split the string into words, to sort them in the reversed order an to translate the task into English :)
