adbbc592886684ff393320eb100449ab3e3e1020
KaterynaT <tkachkv@i.ua>
2016-10-04 12:14:25 +0300

    my commit

--- secondtask.py
+++ secondtask.py
@@ -1,16 +1,18 @@
 """
 We have any integer from 1 to 999 - doesn't matter, in short, this is just a function variable.
 We need to represent it as string of digits like "000001" or "000999" correspondingly.
+The number of digits may vary.
 """
 
-def digstr(number):
+def digstr(number, zeronum = 6 ):
     """
     :param number: any integer from 1 to 999
+    :param zeronum: the number of digits
     :return: a string of digits like "000001" or "000999"
     """
-    a = ("%06d") % number
+    a = str(number).zfill(zeronum)
     return a
 
 if __name__ == "__main__":
-    print(digstr(567))
+    print(digstr(345))
 
