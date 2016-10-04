c7bd6219cee9961053239976ab8781988c934d93
KaterynaT <tkachkv@i.ua>
2016-10-04 16:43:27 +0300

    my commit

--- thirdtask.py
+++ thirdtask.py
@@ -1,11 +1,24 @@
+"""
+We have the list like ["Marun-001-002", "Merin 002-002", etc].
+Create a function (check_files(seek_file_names, folder="./", name_prefix=None))to check if the file exists
+in the directory. Name_prefix is not compulsory. If it is defined, we are into only the names,
+that start with this prefix.
+"""
 
+# -*- coding: utf-8 -*-
 import os
-a = u'/home/katya/Myprojects/Tests'
-def check_files(file_names,folder="/",name_prefix=None):
-    for name in file_names:
-        if os.path.exists(a+folder+name):
+def check_files(seek_file_names, folder="./", name_prefix=None):
+    """
+    :param seek_file_names: the list with potential file names
+    :param folder: wanted folder for seeking the file
+    """
+    for name in seek_file_names:
+        a = os.path.join(folder, name)
+        if os.path.exists(a):
             print ("Exists")
         else:
-            print ("Does not")
-check_files(["thirdtask.py", "firsttask.py"])
+            print ("This file does not exist")
+
+if __name__ == "__main__":
+    check_files(["thirdtask.py", "firsttask.py", "hhhh"])
 
