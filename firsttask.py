35a6a6d253303dc383af77d83c93c19e946d4704
KaterynaT <tkachkv@i.ua>
2016-10-04 09:46:07 +0300

    my first commit

--- firsttask.py
+++ firsttask.py
@@ -1,3 +1,6 @@
+#We have the string like "AAA BBB CCC DDD EEE FFF GGG" 
+#The task is to split the string into words, to sort them in the reversed order an to translate into English
+
 line = "AAA BBB CCC DDD EEE FFF GGG"
 a = line.split(' ')
 print(a)
