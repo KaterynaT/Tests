906a28e2f942ce895260ba7548982f39d6aabc4d
KaterynaT <tkachkv@i.ua>
2016-10-04 11:15:45 +0300

    my commit

--- firsttask.py
+++ firsttask.py
@@ -1,14 +1,14 @@
-'''
+''
 We have the string like "AAA BBB CCC DDD EEE FFF GGG"
 The task is to split the string by any symbol and to sort the contents in the reversed order
-'''
+''
 
 def revline(line, separator=' '):
-    '''
+    ''
     :param line: any string
     :param separator: any symbol, by which we split the string
-    :return: the reversed contents
-    '''
+    :return: a new list with the reversed contents
+    ''
     a = line.split(separator)
     a.sort(reverse=True)
     return a
