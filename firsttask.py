67853eb60c2cf92b5e473c46ac0df67e9c113bd1
KaterynaT <tkachkv@i.ua>
2016-10-04 10:20:42 +0300

    my commit

--- firsttask.py
+++ firsttask.py
@@ -1,12 +1,10 @@
-#We have the string like "AAA BBB CCC DDD EEE FFF GGG"
-#The task is to split the string into words, to sort them in the reversed order an to translate into English
+"""We have the string like "AAA BBB CCC DDD EEE FFF GGG"
+The task is to split the string into words, to sort them in the reversed order"""
+
 def revline(line):
-    #line = "AAA BBB CCC DDD EEE FFF GGG"
     a = line.split(' ')
     a.sort(reverse=True)
     return a
 
 if __name__ == "__main__":
-	print(revline("AAA BBB CCC DDD EEE FFF GGG"))
-else:
-    print('I am running as an imported module with name = %s' % __name__)
+    print(revline("AAA BBB CCC DDD EEE FFF GGG"))
