9ed714ecc1af94d3c648d37e26e5214c23d5ff6a
KaterynaT <tkachkv@i.ua>
2016-10-04 16:58:01 +0300

    my commit

--- thirdtask.py
+++ thirdtask.py
@@ -1,7 +1,7 @@
 """
 We have the list like ["Marun-001-002", "Merin 002-002", etc].
 Create a function (check_files(seek_file_names, folder="./", name_prefix=None))to check if the file exists
-in the directory. Name_prefix is not compulsory. If it is defined, we are into only the names,
+in the directory. Name_prefix is not required. If it is defined, we are into only the names,
 that start with this prefix.
 """
 
