68cd776b5392e6afdfa148b5a0080ba9abfed916
KaterynaT <tkachkv@i.ua>
2016-10-05 10:37:54 +0300

    my commit

--- fourthtask.py
+++ fourthtask.py
@@ -11,15 +11,13 @@ def check_files(seek_file_names, folder="./", name_prefix=None, n=7):
 
 def print_lines(name, n=7):
     with open (name, 'w') as f:
-        i = 0
-        while i < n:
+        for i in range (0, n):
             t = random.randint(4, 590)
             t = str(t)
             f.write(t + '\n')
-            i = i + 1
     f.closed
 
 if __name__ == "__main__":
-    check_files(["thirdtask.py", "firsttask.py", "zuat"])
+    check_files(["thirdtask.py", "firsttask.py", "xuat"])
 
 
