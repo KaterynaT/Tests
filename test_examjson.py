ae8317e05c10a5ec7d6e9a73a8f51da41c67569b
KaterynaT <tkachkv@i.ua>
2016-10-10 12:33:10 +0300

    my commit

---
+++ test_examjson.py
@@ -0,0 +1,23 @@
+import json
+
+
+a_dict = {'Home_addres': ' '}
+
+with open ('examplejs.json') as f:
+    data = json.load(f)
+    for e in f:
+        if e == "Home_addres":
+            print ("there is")
+        else:
+            print ("no information")
+
+
+data.update(a_dict)
+
+with open ('examplejs.json', 'w') as f:
+    json.dump(data, f, indent=2)
+
+
+
+
+
