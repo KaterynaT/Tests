746ebca56a52d0f8eb3a08ed0f66a171ae031587
KaterynaT <tkachkv@i.ua>
2016-10-10 15:41:23 +0300

    my commit

--- test_examjson.py
+++ test_examjson.py
@@ -1,17 +1,37 @@
+import sys
 import json
+sys.path.append('../')
+import unittest
 
 
-a_dict = {'Home_address': ' '}
+class TestJsonFile(unittest.TestCase):
+    def testcheck(self):
+        a_dict = {'Home_address': ' '}
+        with open('examplejs.json') as f:
+            data = json.load(f)
+            if 'Home_addres' in data:
+                print ('there is')
+            else:
+                data.update(a_dict)
 
-with open('examplejs.json') as f:
-    data = json.load(f)
-    if 'Home_address' in data:
-        print ('there is')
-    else:
-        data.update(a_dict)
+        with open('examplejs.json', 'w') as f:
+            json.dump(data, f, indent=2)
 
-with open('examplejs.json', 'w') as f:
-    json.dump(data, f, indent=2)
+    def testcheck2(self):
+        a_dict = {'Another_addr': ' '}
+        with open('examplejs.json') as f:
+            data = json.load(f)
+            if 'Another_addr' in data:
+                print ('there is')
+            else:
+                data.update(a_dict)
+
+        with open('examplejs.json', 'w') as f:
+            json.dump(data, f, indent=2)
+
+
+
+
 
 
 
