d5a8f52672716797cce4b2ca35fee5ae291a9064
KaterynaT <tkachkv@i.ua>
2016-10-13 17:42:12 +0300

    my second lesson

--- lesson2/quantity.py
+++ lesson2/quantity.py
@@ -7,45 +7,52 @@ import json
 
 
 def products(any_json_file):
+    total_quantity = 0
     items = {}
     with open(any_json_file) as f:
         data = json.load(f)
         for i in data:
             quantity = i['inventory'].get('quantity')
+            total_quantity = total_quantity+quantity
             ring_size = int(i["inventory"]['variant'].get('ring_size'))
-            # try:
-            #     items[ring_size] += quantity
-            # except:
-            #     items[ring_size] = quantity
             if ring_size in items:
                 items[ring_size] += quantity
             else:
                 items[ring_size] = quantity
-        print items
-products('ring.json')
+        for i in sorted(items):
+            print(" Ring size %d - Quantity %d" % (i, items[i]))
+        print "Total quantity of items:",total_quantity
 
-with open('ring.json') as f:
-    sum = 0
-    data = json.load(f)
-    w = []
-    o = []
-    dct2 = {}
-    dct = {}
-    for i in data:
-        t = i['inventory']['quantity']
-        sum = sum + t
-        y = int(i['inventory']['variant']['ring_size'])
-        w.append(y)
-        o.append(t)
-    dct2 = dict(zip(w, o))
+    return total_quantity
 
-    for m in w:
-        if m in dct:
-            dct[m] = dct[m] + 1
-        else:
-            dct[m] = 1
-    for i in sorted(dct):
-        print(" Ring size %d : Quantity %d" % (i,dct[i]*dct2[i]))
 
-    print "Toatal quantity of items:  ",sum
+if __name__ == "__main__":
+    products('ring.json')
+
+
+
+# with open('ring.json') as f:
+#     sum = 0
+#     data = json.load(f)
+#     w = []
+#     o = []
+#     dct2 = {}
+#     dct = {}
+#     for i in data:
+#         t = i['inventory']['quantity']
+#         sum = sum + t
+#         y = int(i['inventory']['variant']['ring_size'])
+#         w.append(y)
+#         o.append(t)
+#     dct2 = dict(zip(w, o))
+#
+#     for m in w:
+#         if m in dct:
+#             dct[m] = dct[m] + 1
+#         else:
+#             dct[m] = 1
+#     for i in sorted(dct):
+#         print(" Ring size %d : Quantity %d" % (i,dct[i]*dct2[i]))
+#
+#     print "Toatal quantity of items:  ",sum
 
