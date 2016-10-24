cb52c7388fb574eeef1379a1ed9d51acd1e022cf
KaterynaT <tkachkv@i.ua>
2016-10-24 18:13:01 +0300

    my second lesson

--- lesson2/fib.py
+++ lesson2/fib.py
@@ -1,3 +1,7 @@
+"""
+Write down the fibonacci function, using the "memorize" decorator for decreasing the algorithm complexity.
+"""
+
 from functools32 import wraps
 
 
@@ -7,22 +11,23 @@ def memorize(max_length=5):
     def real_decorator(function):
         @wraps(function)
         def wrapper(args):
-            # print(max_length, len(memo))
             if memo.get(args, None):
                 return memo[args]
             else:
-                rv = function(args)
-                memo[args] = rv
+                remembered_fibonacci_number = function(args)
+                memo[args] = remembered_fibonacci_number
                 if len(memo) == max_length:
                     del(memo[memo.keys()[0]])
-            return rv
+            # print remembered_fibonacci_number
+            # print memo
+            return remembered_fibonacci_number
         return wrapper
     return real_decorator
 
 
 @memorize()
 def fibonacci(n):
-    print('***')
+    # print('***')
     if n < 2:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
@@ -30,4 +35,4 @@ def fibonacci(n):
 
 print (fibonacci(19))
 print('=' * 10)
-print (fibonacci(30))
+print (fibonacci(34))
