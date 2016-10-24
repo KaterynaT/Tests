ed4090bc4b617a72da385e2e58eef974ad5c87ba
KaterynaT <tkachkv@i.ua>
2016-10-24 14:29:06 +0300

    my fifth lesson

---
+++ lesson6/names.py
@@ -0,0 +1,12 @@
+
+import requests
+import json
+
+
+def find_names(link = "https://api.github.com/repositories"):
+    file = requests.get(link)
+    content = json.loads(file.content)
+    for i in content:
+        name = i.get('name')
+        print name
+find_names()
