68856f6476d77747f9577e5e5918fa7d8f453ef7
KaterynaT <tkachkv@i.ua>
2016-10-25 11:45:11 +0300

    my fourth lesson

--- lesson4/mro.py
+++ lesson4/mro.py
@@ -1,28 +1,18 @@
-# -*- coding: utf-8 -*-
+"""
+Make "Mixin" for adding to your Class the function "Name_of_class",
+which would return the name of the current Class
+"""
+
+class BaseClass:
+    def Name_of_class(self):
+        print "Base Class"
 
 class Mixin(object):
-    @classmethod
     def name_of_class(self):
         print "You've added mixin to %s" % self.__class__.__name__
 
 class MyClass(Mixin):
-    def name_of_class(self):
-        print "This is my class"
-        super(MyClass,self).name_of_class()
+    pass
 
-Mixin.name_of_class()
 obj = MyClass()
 obj.name_of_class()
-
-#
-# # Родительский класс
-#
-# class A(object):
-#     def __init__(self):
-#         print(u'конструктор класса A')
-#
-# # Потомок класса А
-# class B(A):
-#     def __init__(self):
-#         print(u'конструктор класса B')
-#         super(B, self).__init__()
