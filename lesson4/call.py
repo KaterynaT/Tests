40891bd9524bc253168c0098af0bfaec37c596de
KaterynaT <tkachkv@i.ua>
2016-10-27 12:35:47 +0300

    my fourth lesson

--- lesson4/call.py
+++ lesson4/call.py
@@ -1,23 +1,12 @@
 # -*- coding: utf-8 -*-
-#
-# class Name(object):
-#     def __call__(self, a):
-#         if type(a) == int:
-#             print "Это integer"
-#         elif type(a) == str:
-#             print "Это string"
-#         elif type(a) == float:
-#             print "Это float"
-#         print a
-#         return a
-#
-#
-# f = Name()
-# f(9.9)
 
 
 class A:
-    def printint(self):
+    @staticmethod
+    def printint(*args):
+        """
+        :param args: any argument, that can be entered
+        """
         print "Это integer"
     def printfloat(self):
         print "Это float"
@@ -25,18 +14,18 @@ class A:
         print "Это string"
 
     def __call__(self, name):
-        # print name
-        # print ("print%s" % type(name).__name__)
+        """
+        :param name: any data(integer, float, string etc.)
+        """
         try:
             getattr(self, "print%s" % type(name).__name__).__call__()
         except:
             print "Введенный тип данных не поддерживается"
-            return int
+            # return int
 
 class B(A):
     def printlist(self):
         print "Это список"
 
 
-obj = B()
-obj(9.6)
+A()(5)
